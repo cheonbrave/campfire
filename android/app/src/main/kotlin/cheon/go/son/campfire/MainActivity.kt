73d1be8d8a62a3f8bc81c35612df14604c6a5245
cheonbrave <cheonbrave@gmail.com>
2020-03-05 22:25:24 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/cheon/go/son/campfire/MainActivity.kt
@@ -0,0 +1,12 @@
+package cheon.go.son.campfire
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
